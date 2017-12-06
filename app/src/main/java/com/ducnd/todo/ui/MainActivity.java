3464589ca2f9cce4f4392aebac3237e1a75d5bb8
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2017-12-06 20:19:03 +0700

    init project

---
+++ app/src/main/java/com/ducnd/todo/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ducnd.todo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ducnd.todo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
